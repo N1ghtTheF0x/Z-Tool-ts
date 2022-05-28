0d1bdc8b28c21b0607c174095b692e7233c925c1
Night The Fox <60095145+N1ghtTheF0x@users.noreply.github.com>
2022-05-28 04:12:05 +0200

    Delete build directory

--- build/src/gbs/imageglyphflg.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ImageGlyphFlg = void 0;
-var ImageGlyphFlg;
-(function (ImageGlyphFlg) {
-    ImageGlyphFlg[ImageGlyphFlg["No"] = 0] = "No";
-    ImageGlyphFlg[ImageGlyphFlg["Yes"] = 1] = "Yes";
-})(ImageGlyphFlg = exports.ImageGlyphFlg || (exports.ImageGlyphFlg = {}));
